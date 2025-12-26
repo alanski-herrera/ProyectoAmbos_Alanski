21ede6d342afa8278545a3c084e81bf94a65262a
Alan Herrera <109108802+EzeSky24@users.noreply.github.com>
2025-12-26 16:43:07 -0300

    agrege .gitignore

---
+++ ProyectoAmbos_Alanski/Controllers/UniformeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAmbos_Alanski.Controllers
+{
+    public class UniformeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
