2a025c5e5ce8d5b0a641d018cb48a544f51759a9
alanski-herrera <alanherrera.janis@gmail.com>
2025-12-26 16:50:59 -0300

    agrego ClienteController

---
+++ ProyectoAmbos_Alanski/Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoAmbos_Alanski.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
